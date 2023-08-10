029732dadb36116d1c4c9c8ce21a0b6f8ed3f324
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2023-08-10 01:00:00 -0300

    chore: initialize project

---
+++ Core/Program.fs
@@ -0,0 +1,24 @@
+﻿open Microsoft.Xna.Framework
+
+type SpaceEscapeGame() as this =
+    inherit Game()
+
+    let graphics = new GraphicsDeviceManager(this)
+
+    override this.Initialize () =
+        this.IsMouseVisible <- true
+
+        base.Initialize()
+
+    override _.LoadContent () =
+        base.LoadContent()
+
+    override _.Update gameTime =
+        base.Update gameTime
+    
+    override this.Draw gameTime =
+        this.GraphicsDevice.Clear Color.CornflowerBlue
+
+        base.Draw gameTime
+
+(new SpaceEscapeGame()).Run()
